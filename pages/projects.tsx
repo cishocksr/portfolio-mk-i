5e79306062954ecb2f9a6bcfe0c8c97cea8a89cc
Chris Shockley <cishockleysr@gmail.com>
2021-04-20 07:42:03 -0400

    contact form fully functional with emailjs

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import ProjectCard from "../components/projectCard.component";
-import ProjectsNavbar from "../components/project-navbar.component";
-import { projects as projectsData } from "../helpers/data";
-import { Category } from "../helpers/types";
-import { motion } from "framer-motion";
-import { fadeInUp, stagger } from "../helpers/animations";
+import { useState } from 'react';
+import ProjectCard from '../components/projectCard.component';
+import ProjectsNavbar from '../components/project-navbar.component';
+import { projects as projectsData } from '../data';
+import { Category } from '../helpers/types';
+import { motion } from 'framer-motion';
+import { fadeInUp, stagger } from '../helpers/animations';
 
 const Projects = () => {
   const [projects, setProjects] = useState(projectsData);
-  const [active, setActive] = useState("all");
+  const [active, setActive] = useState('all');
 
   const [showDetail, setShowDetail] = useState<number | null>(null);
 
-  const handlerFilterCategory = (category: Category | "all") => {
-    if (category === "all") {
+  const handlerFilterCategory = (category: Category | 'all') => {
+    if (category === 'all') {
       setProjects(projectsData);
       setActive(category);
       return;
@@ -27,23 +27,23 @@ const Projects = () => {
   };
 
   return (
-    <div className="px-5 py-2 overflow-y-scroll" style={{ height: "65vh" }}>
+    <div className='px-5 py-2 overflow-y-scroll' style={{ height: '65vh' }}>
       <ProjectsNavbar
         handlerFilterCategory={handlerFilterCategory}
         active={active}
       />
       <motion.div
         variants={stagger}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        className="relative grid grid-cols-12 gap-4 my-3"
+        initial='initial'
+        animate='animate'
+        exit='exit'
+        className='relative grid grid-cols-12 gap-4 my-3'
       >
         {projects.map((project) => (
           <motion.div
             variants={fadeInUp}
             key={project.name}
-            className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200"
+            className='col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200'
           >
             <ProjectCard
               project={project}
