0a31a83d440c7a5d9062a3814a35302860760cf5
Chris Shockley <cishockleysr@gmail.com>
2021-04-20 23:03:52 -0400

    fixed gitignore

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { services } from '../../data';
+import { services } from '../../data/data';
 
 export default (res: NextApiResponse, req: NextApiRequest) => {
   res.status(200).json({ services });
